af901e9cd991c8654e59821d36e7478c88fd5002
ZeusHo <91323637+ZeusHo@users.noreply.github.com>
2021-12-30 19:52:37 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,13 +1,13 @@
 
 import telebot
 from bs4 import BeautifulSoup
-
+import os
 import requests
 
 url = "https://www.tgju.org/profile/sekeb"
 req = requests.get(url)
 soup = BeautifulSoup(req.text, "html.parser")
-
+token = os.environ['5057221416:AAEPGZWbmS0v2zTcRVJzs9vxIt-wsBwkFvg']
 
 for web in soup.find_all(class_='price',limit=1):
   
@@ -17,14 +17,7 @@ API_TOKEN = "5057221416:AAEPGZWbmS0v2zTcRVJzs9vxIt-wsBwkFvg"
 bot = telebot.TeleBot(API_TOKEN)
 
 @bot.message_handler(commands=['help', 'start','seke'])
-# def send_welcome(message):
-#     bot.reply_to(message, """\
-# Hi there, I am EchoBot.
-# I am here to echo your kind words back to you. Just say anything nice and I'll say the exact same thing to you!\
-# """)
 
-
-# Handle all other messages with content_type 'text' (content_types defaults to ['text'])
 @bot.message_handler(func=lambda message: True)
 def seke(message):
     bot.reply_to(message, havij.text)
